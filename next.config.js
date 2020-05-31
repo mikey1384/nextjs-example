a6cb9546727dd74cc415e9710931431c018005ba
mikey1384 <mikey1384@gmail.com>
2020-05-31 22:58:19 +0900

    minor fix

--- next.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  env: {
-    FIREBASE_PROJECT_ID: "lumine-network",
-    FIREBASE_PRIVATE_KEY: "AIzaSyBjUuIAFOmi9yF87M3sAe8G26YCi19X01E",
-    FIREBASE_CLIENT_EMAIL: "mikey1384@gmail.com",
-  },
-};
